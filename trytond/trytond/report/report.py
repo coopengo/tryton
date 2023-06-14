9d9e483049cb2799a7fc81b34da9d212706651e5
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:03:36 +0200

    [trytond][ANALYSE] Add support for post 2013 docx and xlsx in Report [CUSTOM]
    Add support for post-2013 Office formats (docx, xlsx) in the report
    engine. Extends report.py MIME type mapping to handle modern Office
    document formats for report output conversion.
    
    Files: report/report.py
    Extraction ref: C17 (partial — report format support)

--- trytond/trytond/report/report.py
+++ trytond/trytond/report/report.py
@@ -83,6 +83,7 @@ FORMAT2EXT = {
     'doc95': 'doc',
     'docbook': 'xml',
     'docx7': 'docx',
+    'docx': 'docx',
     'ooxml': 'xml',
     'latex': 'ltx',
     'sdc4': 'sdc',
@@ -97,6 +98,7 @@ FORMAT2EXT = {
     'xhtml': 'html',
     'xls5': 'xls',
     'xls95': 'xls',
+    'xlsx': 'xlsx',
     }
 
 TIMEDELTA_DEFAULT_CONVERTER = {
