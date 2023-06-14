bfe7ff95f54b583caf4b324fdadcf4a07bf40b4b
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:07:16 +0200

    [sao] Activate double click on editable treeviews (#134) [CUSTOM]
    Fix #22200

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -1769,9 +1769,8 @@
                 }
                 td.attr('headers', column.header.attr('id'));
                 td.on('click keypress', {'index': i}, on_click);
-                if (!this.tree.editable) {
-                    td.dblclick(this.switch_row.bind(this));
-                } else {
+                td.dblclick(this.switch_row.bind(this));
+                if (this.tree.editable) {
                     if (column.attributes.required) {
                         td.addClass('required');
                     }
