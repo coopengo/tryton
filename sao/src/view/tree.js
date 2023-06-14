1da5b9fe6141e6096d04a5aecf224e5cc0719469
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:06:38 +0200

    [sao] Tree: do not call focus if widget does not define it [CUSTOM]
    Fix #8720

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -2325,7 +2325,7 @@
                     }
                 }
             }
-            if (focus_widget) {
+            if (focus_widget && focus_widget.focus) {
                 focus_widget.focus();
             }
         },
