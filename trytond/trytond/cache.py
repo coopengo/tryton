cbcebf91abb8027f09ad738acbb5b2032384145f
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:03:33 +0200

    [trytond][ANALYSE] Guardrails [CUSTOM]
    Add guardrails for cache and modelstorage edge cases. Hardens cache.py
    with additional checks for missing/corrupt cache entries, and
    modelstorage.py with a guard against accessing records with invalid IDs.
    
    Files: cache.py, model/modelstorage.py
    Extraction ref: C50a (partial — cache robustness) + C50c (partial)

--- trytond/trytond/cache.py
+++ trytond/trytond/cache.py
@@ -212,9 +212,12 @@ class MemoryCache(BaseCache):
             cache.move_to_end(key)
             self.hit += 1
             return deepcopy(result)
-        except (KeyError, TypeError):
+        except KeyError:
             self.miss += 1
             return default
+        except TypeError:
+            # JCA : Properly crash on type error
+            raise
 
     def set(self, key, value):
         key = self._key(key)
@@ -232,7 +235,8 @@ class MemoryCache(BaseCache):
         try:
             cache[key] = (expire, deepcopy(value))
         except TypeError:
-            pass
+            # JCA : Do not silently fail when trying to use a non hashable key
+            raise
         return value
 
     def clear(self):
