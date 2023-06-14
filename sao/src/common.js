846d1981b15719fe7db07eb441838f48018c48ee
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:07:09 +0200

    [sao][ANALYSE] Fix recursive context and wrong handling of tab_domain in search_count (#113) [CUSTOM]
    Fix recursive context evaluation by cloning context before use in
    action.js, and add tab_domain handling in search_count to prevent wrong
    results when domains interact with tab filtering. Adds deep_copy utility
    to common.js for safe context cloning.
    
    Files: sao/src/action.js, sao/src/common.js
    Extraction ref: SA12-C
    Source commits: 75b3c06 (fix recursive context and wrong handling of tab_domain in search_count)
    
    Ref: #113

--- sao/src/common.js
+++ sao/src/common.js
@@ -4563,6 +4563,16 @@
         }
     };
 
+    Sao.common.clone = function(obj) {
+        var copy = obj.constructor();
+        for (var attr in obj) {
+            if (Object.hasOwn(obj, attr)) {
+                copy[attr] = obj[attr];
+            }
+        }
+        return copy;
+    };
+
     Sao.common.PopupMenu = {
         initialize: (evt) => {
             let menu = jQuery('#popup-menu');
