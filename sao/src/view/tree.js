3013e3531bf04b740135a3d0b8daacdb4c71ea0a
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:06:38 +0200

    [sao][Guardrail] Tree: do not call focus if widget does not define it [CUSTOM]
    Add guard in tree view to check if a widget defines the focus method
    before calling it, preventing crashes on widgets that don't support focus.
    
    Files: sao/src/view/tree.js
    Extraction ref: SA7-C
    Source commits: 1da5b9f (tree: do not call focus if widget does not define it)

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -2449,7 +2449,7 @@
                     }
                 }
             }
-            if (focus_widget) {
+            if (focus_widget && focus_widget.focus) {
                 focus_widget.focus();
             }
         },
