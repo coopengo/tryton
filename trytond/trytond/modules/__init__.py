bfb98e1dd50a7379a78e64d605d3897b583a0c40
Nicolas Évrard <nicoe@users.noreply.github.com>
2023-10-12 10:58:12 +0200

    [trytond][ANALYSE] to remove ? Add INDEX creation SQL dump [CUSTOM] (#49)
    Add dump_indexes() method to table handlers and support CONCURRENTLY
    for index creation/drop. Allows generating SQL dump of index creation
    statements for offline analysis or migration tooling.
    
    Also removes the "skip indexes creation" log message and adds tempfile
    import for future use.
    
    Files: backend/postgresql/table.py, backend/sqlite/table.py, backend/table.py, commandline.py, modules/__init__.py
    Extraction ref: C39 (Index management)
    
    Ref: #49

--- trytond/trytond/modules/__init__.py
+++ trytond/trytond/modules/__init__.py
@@ -7,6 +7,7 @@ import os
 import sys
 import os.path
 import pkgutil
+import tempfile
 from collections import defaultdict
 from glob import iglob
 
@@ -325,8 +326,6 @@ def load_module_graph(graph, pool, update=None, lang=None, indexes=None):
                     [ir_configuration.series], [__series__]))
             if indexes or indexes is None:
                 create_indexes(concurrently=False)
-            else:
-                logger.info('skip indexes creation')
             for model_name in models_to_update_history:
                 model = pool.get(model_name)
                 if model._history:
