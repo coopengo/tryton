7f6c7ad0608fdf54dac30bea2a8e7ba224b6e43c
Nicolas Évrard <nicoe@users.noreply.github.com>
2023-12-11 10:55:36 +0100

    [trytond][Main] Allow to activate stat thread by configuration [CUSTOM] (#100)
    Make the stat reporting thread configurable. Adds configuration option
    to enable/disable the status thread that reports server statistics,
    defaulting to disabled. Prevents unnecessary resource usage in
    production deployments.
    
    Files: status.py, config.py
    Extraction ref: C14
    Original PR: #100

--- trytond/trytond/config.py
+++ trytond/trytond/config.py
@@ -6,6 +6,8 @@ import os
 import urllib.parse
 from getpass import getuser
 
+import __main__ as main
+
 __all__ = ['config', 'get_hostname', 'get_port', 'split_netloc',
     'parse_listen', 'parse_uri']
 logger = logging.getLogger(__name__)
@@ -104,6 +106,8 @@ class TrytonConfigParser(configparser.ConfigParser):
         self.set('bus', 'cache_timeout', '5')
         self.set('bus', 'select_timeout', '5')
         self.add_section('html')
+        self.add_section('custom')
+        self.set('custom', 'enable_stat_thread', 'False')
         if overrides:
             self.update_etc(configfile=overrides)
         self.update_environ()
@@ -180,5 +184,6 @@ class TrytonConfigParser(configparser.ConfigParser):
         if not overrides:
             return self
         return TrytonConfigParser(overrides=overrides)
+
 
 config = TrytonConfigParser().apply_overriden_defaults()
