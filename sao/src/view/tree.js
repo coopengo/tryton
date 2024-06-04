45fb3e8694482581ffe19c3b97c787d21226a3f7
Nicolas Évrard <nicoe@users.noreply.github.com>
2024-06-04 17:53:06 +0200

    Correctly compute column visibility [CUSTOM] (#226)

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -2682,7 +2682,6 @@
             this.suffixes = [];
             this.header = null;
             this.footers = [];
-            this._visible_header = true;
         },
         get field_name() {
             return this.attributes.name;
@@ -2713,10 +2712,8 @@
                 this.field.set_state(record);
                 var state_attrs = this.field.get_state_attrs(record);
                 if (state_attrs.invisible) {
-                    this._visible_header = false;
                     cell.hide();
                 } else {
-                    this._visible_header = true;
                     cell.show();
                 }
             };
@@ -2744,19 +2741,18 @@
             for (const cell of cells) {
                 if (visible) {
                     cell.show();
-                    this._visible_header = true;
                     cell.removeClass('invisible');
                 } else {
                     cell.hide();
-                    this._visible_header = false;
                     cell.addClass('invisible');
                 }
             }
         },
         get_visible: function() {
             // 480px is bootstrap's screen-xs-max
-            return (window.visualViewport.width > 480) && this._visible_header;
-        },
+            return ((window.visualViewport.width > 480) &&
+                !this.header.hasClass('invisible'));
+        }
     });
 
     Sao.View.Tree.TextColum = Sao.class_(Sao.View.Tree.CharColumn, {
@@ -3252,7 +3248,6 @@
             this.type = 'button';
             this.attributes = attributes;
             this.footers = [];
-            this._visible_header = true;
         },
         render: function(record, el) {
             var button = new Sao.common.Button(this.attributes, el, 'btn-sm');
@@ -3278,17 +3273,16 @@
             for (const cell of cells) {
                 if (visible) {
                     cell.show();
-                    this._visible_header = true;
                     cell.removeClass('invisible');
                 } else {
                     cell.hide();
-                    this._visible_header = false;
                     cell.addClass('invisible');
                 }
             }
         },
         get_visible: function() {
-            return this._visible_header && !this.header.hasClass('invisible');
+            return ((window.visualViewport.width > 480) &&
+                !this.header.hasClass('invisible'));
         },
         button_clicked: function(event) {
             var record = event.data[0];
