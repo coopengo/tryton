ee2dcffa6aa8e88ccb02892f9444cec1ffc9df8b
Jean Cavallo <jean.cavallo@hotmail.fr>
2024-07-10 17:52:25 +0200

    sao: [6.8-patches] Better usage of vertical space [CUSTOM] (#252)
    Fix PCLAS-1055

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -675,7 +675,9 @@ function eval_pyson(value){
                         // usually give a better (though non-perfect) UI
                         // this._grid_rows.push(
                         //      `minmax(min-content, ${this._row}fr)`);
-                        this._grid_rows.push('min-content');
+                        // this._grid_rows.push('min-content');
+                        // this._grid_rows.push('1fr');
+                        this._grid_rows.push('auto');
                     } else {
                         this._grid_rows.push('min-content');
                     }
@@ -2793,6 +2795,7 @@ function eval_pyson(value){
                     'readonly': 'readonly',
                 });
             widget.css('min-height', this.el.height());
+            widget.css('max-height', this.el.height());
             return widget;
         }
     });
@@ -5221,7 +5224,7 @@ function eval_pyson(value){
 
     Sao.View.Form.Dict = Sao.class_(Sao.View.Form.Widget, {
         class_: 'form-dict',
-        expand: true,
+        expand: false,
         init: function(view, attributes) {
             Sao.View.Form.Dict._super.init.call(this, view, attributes);
 
