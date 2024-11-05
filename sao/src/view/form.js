0a41f95eaaf039708d1536b9e62a94ec2554554d
Nicolas Évrard <nicoe@users.noreply.github.com>
2024-11-05 17:09:45 +0100

    [sao][ANALYSE] Find new key names using the definition from the widget's field [CUSTOM] (#335)
    Fix Dict widget new_key_names lookup to use the definition from the
    widget's field rather than relying on the global model definition. This
    corrects key name resolution when the Dict widget is used with field
    definitions that differ from the model's default.
    
    Files: sao/src/view/form.js
    Extraction ref: SA5-C
    Source commits: 4c1754c (find new key names using definition from widget's field)
    
    Ref: #335

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -4611,6 +4611,13 @@ function eval_pyson(value){
                 }
 
                 var new_group = record.field_get_client(this.field_name);
+                if (new_group && new_group != this.screen.group) {
+                    this.screen.set_group(new_group);
+                    if ((this.screen.current_view.view_type == 'form') &&
+                        this.screen.group.length) {
+                        this.screen.current_record = this.screen.group[0];
+                    }
+                }
 
                 // [Coog specific]
                 // > multi_mixed_view see tryton/8fa02ed59d03aa52600fb8332973f6a88d46d8c0
@@ -4619,13 +4626,7 @@ function eval_pyson(value){
                         this.set_invisible(this.visible);
                     }
                 }
-                if (new_group && new_group != this.screen.group) {
-                    this.screen.set_group(new_group);
-                    if ((this.screen.current_view.view_type == 'form') &&
-                        this.screen.group.length) {
-                        this.screen.current_record = this.screen.group[0];
-                    }
-                }
+
                 var domain = [];
                 var size_limit = null;
                 if (record) {
@@ -6393,7 +6394,7 @@ function eval_pyson(value){
 
             var value = field.get_client(record);
             var new_key_names = Object.keys(value).filter(
-                e => !this.fields[e]);
+                e => !this.field.keys[e]);
 
             var prm;
             if (!jQuery.isEmptyObject(new_key_names)) {
