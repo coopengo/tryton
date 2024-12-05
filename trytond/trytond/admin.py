58be92990de1cd4d0532ed67867336148ce890d9
Nicolas Évrard <nicoe@users.noreply.github.com>
2024-12-05 17:25:14 +0100

    [coog-24.41] Allow trytond-admin set password of any user. Activate user if inactive. [PREVIEW] (#365)
    https://bugs.tryton.org/13630
    Fix CCOOG-1137

--- trytond/trytond/admin.py
+++ trytond/trytond/admin.py
@@ -191,14 +191,17 @@ def run(options):
             if not password and not options.reset_password:
                 while True:
                     password = getpass(
-                        '"admin" password for "%s": ' % db_name)
-                    password2 = getpass('"admin" password confirmation: ')
+                        f'"{options.login}" password for "{db_name}": ')
+                    password2 = getpass(
+                        f'"{options.login}" password confirmation: ')
                     if password != password2:
-                        sys.stderr.write('"admin" password confirmation '
-                            'doesn\'t match "admin" password.\n')
+                        sys.stderr.write(
+                            f'"{options.login}" password confirmation '
+                            f'doesn\'t match "{options.login}" password.\n')
                         continue
                     if not password:
-                        sys.stderr.write('"admin" password is required.\n')
+                        sys.stderr.write(
+                            f'"{options.login}" password is required.\n')
                         continue
                     break
 
@@ -212,17 +215,35 @@ def run(options):
                     Configuration = pool.get('ir.configuration')
                     configuration = Configuration(1)
                     with inactive_records():
-                        admin, = User.search([('login', '=', 'admin')])
+                        users = User.search([('login', '=', options.login)])
+                        if not users:
+                            sys.stderr.write(
+                                f'User with login "{options.login}"'
+                                ' not found.\n')
+                            break
+                        user, = users
+
+                        if not user.active:
+                            activate = input(
+                                f'Warning: User "{options.login}" is '
+                                'inactive. Proceeding will activate the user. '
+                                'Continue? (Y/n): ').strip().lower() or 'y'
+                            if activate == 'y':
+                                user.active = True
+                                sys.stderr.write(
+                                    f'User "{options.login}" activated.\n')
+                            else:
+                                sys.stderr.write('Activation cancelled\n')
 
                     if email is not None:
-                        admin.email = email
+                        user.email = email
                     if init[db_name] or options.password:
                         configuration.language = main_lang
                         if not options.reset_password:
-                            admin.password = password
-                    admin.save()
+                            user.password = password
+                    user.save()
                     if options.reset_password:
-                        User.reset_password([admin])
+                        User.reset_password([user])
                     if options.hostname is not None:
                         configuration.hostname = options.hostname or None
                     configuration.save()
