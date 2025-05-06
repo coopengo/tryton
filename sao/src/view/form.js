862a3193f1ad5b36c1f22f8a973fc3b860db9e96
Nicolas Évrard <nicoe@openhex.org>
2025-05-06 21:28:12 +0200

    Display JSONB fields [CUSTOM]
    Fix PCLAS-1599

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -6600,6 +6600,43 @@ function hide_x2m_body(widget) {
         }
     });
 
+    Sao.View.Form.JSON = Sao.class_(Sao.View.Form.Widget, {
+        class_: 'form-json',
+        expand: true,
+        init: function(view, attributes) {
+            Sao.View.Form.JSON._super.init.call(this, view, attributes);
+            this.el = jQuery('<div/>', {
+                'class': this.class_,
+            });
+            this.group = jQuery('<div/>', {
+                'class': 'input-group',
+            }).appendTo(this.el);
+            this.input = this.labelled = jQuery('<textarea/>', {
+                'class': 'form-control input-sm mousetrap',
+                'name': attributes.name,
+            }).appendTo(this.group);
+        },
+        set_readonly: function(readonly) {
+            Sao.View.Form.JSON._super.set_readonly.call(this, readonly);
+            this.input.prop('readonly', readonly);
+        },
+        display: function() {
+            Sao.View.Form.JSON._super.display.call(this);
+            let record = this.record;
+            if (record) {
+                let value = record.field_get_client(this.field_name);
+                value = JSON.stringify(value, null, 2);
+                if (this.attributes.yexpand) {
+                    this.input.css('height', value.split('\n').length * 2.5 + 2 + "ex");
+                }
+                this.input.val(value);
+            } else {
+                this.input.val('');
+            }
+            return jQuery.when();
+        },
+    });
+
     Sao.View.FormXMLViewParser.WIDGETS = {
         'binary': Sao.View.Form.Binary,
         'boolean': Sao.View.Form.Boolean,
@@ -6633,5 +6670,6 @@ function hide_x2m_body(widget) {
         'timedelta': Sao.View.Form.TimeDelta,
         'timestamp': Sao.View.Form.DateTime,
         'url': Sao.View.Form.URL,
+        'json': Sao.View.Form.JSON,
     };
 }());
