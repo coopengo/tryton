0a94a11cf808bfb6dabfa8d2ddcd60a7d4aa7a4b
Nicolas Évrard <nicoe@users.noreply.github.com>
2025-06-05 21:27:08 +0200

    Sao: customize name of tab for forms only [CUSTOM] (#452)
    Fix PCLAS-1694

--- sao/src/tab.js
+++ sao/src/tab.js
@@ -349,9 +349,6 @@
             return jQuery.when();
         },
         set_name: function(name) {
-            if (name != this.attributes.name) {
-                name = `${this.attributes.name}: ${name}`;
-            };
             this.name_short_el.text(name.split(' / ').pop());
             this.name_long_el.text(name);
             this.name_el.attr('title', name);
