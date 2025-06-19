nicoe/tryton-coopengo

[trytond] Add basic liveness probe [CUSTOM]
PR: #197

trytond/trytond/cron.py
@@ -1,8 +1,10 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import logging
+import sys
 import threading
 import time
+from pathlib import Path
 
 from trytond.pool import Pool
 from trytond.transaction import Transaction
@@ -13,7 +15,16 @@ logger = logging.getLogger(__name__)
 
 def run(options):
     threads = []
+
     for name in options.database_names:
+        if options.check:
+            path = Path(f'/tmp/cron_canary_{name}')
+            if path.exists():
+                logger.info(f'canary file exists for {name}')
+                path.unlink()
+                return
+            logger.error(f'No cron canary file for {name}')
+            sys.exit(1)
         thread = threading.Thread(target=Pool(name).init)
         thread.start()
         threads.append(thread)
@@ -40,6 +51,7 @@ def run(options):
             logger.info('start thread for "%s"', db_name)
             thread.start()
             threads[db_name] = thread
+            Path(f'/tmp/cron_canary_{db_name}').touch()
         if options.once:
             break
         time.sleep(60)

coopengo/tryton

trytond/trytond/cron.py
@@ -7,6 +7,7 @@ import sys
 import time
 from concurrent import futures
 from multiprocessing import cpu_count
+from pathlib import Path
 
 from trytond.pool import Pool
 from trytond.transaction import Transaction
@@ -34,7 +35,16 @@ def run(options):
     with futures.ProcessPoolExecutor(**executor_options) as executor:
         while True:
             for database_name in options.database_names:
+                path = Path(f'/tmp/cron_canary_{database_name}')
+                if options.check:
+                    if path.exists():
+                        logger.info(f'canary file exists for {database_name}')
+                        path.unlink()
+                        return
+                    logger.error(f'No cron canary file for {database_name}')
+                    sys.exit(1)
                 executor.submit(run_cron, database_name)
+                path.touch()
             if options.once:
                 break
             else: