267e79bcaa0829ca4e2e6e324ff98febb6fa33b3
Jean Cavallo <jean.cavallo@hotmail.fr>
2025-07-08 17:15:09 +0200

    [sao][ANALYSE] Set active_ids when clicking a button on a non-selected line [CUSTOM] (#464)
    Set active_ids in screen context when clicking a button on a non-selected
    line in a tree view. Without this, button actions on unselected rows
    would operate with an empty active_ids, causing server-side errors or
    incorrect behavior.
    
    Files: sao/src/screen.js
    Extraction ref: SA7-C
    Source commits: 89d7e32 (set active_ids when clicking button on non-selected line)
    
    Ref: #464

--- sao/src/screen.js
+++ sao/src/screen.js
@@ -2183,7 +2183,10 @@
                                 Sao.Action.execute(act, {
                                     model: this.model_name,
                                     id: this.current_record.id,
-                                    ids: ids
+                                    ids: (
+                                        (ids.length == 0 && this.current_record.id)
+                                        ? [this.current_record.id]
+                                        : ids),
                                 }, null, this.context, true));
                         }
                     }
@@ -2198,7 +2201,10 @@
                         Sao.Action.execute(action, {
                             model: this.model_name,
                             id: this.current_record.id,
-                            ids: ids
+                            ids: (
+                                (ids.length == 0 && this.current_record.id)
+                                ? [this.current_record.id]
+                                : ids),
                         }, null, this.context, true));
                 }
                 return jQuery.when.apply(jQuery, prms).then(() => {
