b369a5c74a75c022eb5d752ff66e1b0b8f4d2289
Jean Cavallo <jean.cavallo@hotmail.fr>
2025-07-08 17:15:09 +0200

    sao: [25.14] Set active_ids when clicking a button on a non-selected line [CUSTOM] (#464)
    Issue link: [https://coopengo.atlassian.net/browse/PCLAS-1747](https://coopengo.atlassian.net/browse/PCLAS-1747)
    
    Fix PCLAS-1747

--- sao/src/screen.js
+++ sao/src/screen.js
@@ -2102,7 +2102,10 @@
                                 Sao.Action.execute(act, {
                                     model: this.model_name,
                                     id: this.current_record.id,
-                                    ids: ids
+                                    ids: (
+                                        (ids.length == 0 && this.current_record.id)
+                                        ? [this.current_record.id]
+                                        : ids),
                                 }, null, this.context, true));
                         }
                     }
@@ -2117,7 +2120,10 @@
                         Sao.Action.execute(action, {
                             model: this.model_name,
                             id: this.current_record.id,
-                            ids: ids
+                            ids: (
+                                (ids.length == 0 && this.current_record.id)
+                                ? [this.current_record.id]
+                                : ids),
                         }, null, this.context, true));
                 }
                 return jQuery.when.apply(jQuery, prms).then(() => {
