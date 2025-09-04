177d178fdb1bcc9b8812226ecc9fb74fc627c851
Nicolas Évrard <nicoe@users.noreply.github.com>
2025-09-04 10:19:32 +0200

    sao: [25.14] Allow to reorder tabs [PREVIEW] (#479)
    Fix CCOOG-1246

--- sao/src/common.js
+++ sao/src/common.js
@@ -4556,4 +4556,7 @@
         },
     };
 
+    Sao.common.transparent_png = document.createElement('img');
+    Sao.common.transparent_png.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+
 }());
