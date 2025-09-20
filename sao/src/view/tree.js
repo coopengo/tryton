383056343ff37edca04ac41677f8df2802db65c8
Nicolas Évrard <nicoe@openhex.org>
2025-09-20 10:58:35 +0200

    Show copy while manipulating DOM nodes [PREVIEW]
    This trick allows to prevent forced reflows
    
    Fix PCLAS-1829

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -2375,6 +2375,16 @@
         },
         set_editable: function() {
             var focus_widget = null;
+            let table_node = this.el[0].parentNode;
+            let tr_copy = this.el[0].cloneNode(true);
+            Object.keys(tr_copy).forEach(key => {
+                tr_copy.addEventListener(key.slice(2), (evt) => {
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                });
+            });
+            table_node.replaceChild(tr_copy, this.el[0]);
+            let display_prms = [];
             for (var i = 0, len=this.tree.columns.length; i < len; i++) {
                 var td = this._get_column_td(i);
                 var col = this.tree.columns[i];
@@ -2391,7 +2401,7 @@
                     var editable_el = this.get_editable_el(td);
                     editable_el.append(widget.el);
                     editable_el.data('widget', widget);
-                    widget.display(this.record, col.field);
+                    display_prms.push(widget.display(this.record, col.field));
 
                     var static_el = this.get_static_el(td);
                     static_el.hide();
@@ -2404,6 +2414,9 @@
                     }
                 }
             }
+            jQuery.when.apply(jQuery, display_prms).done(() => {
+                table_node.replaceChild(this.el[0], tr_copy);
+            });
             if (focus_widget && focus_widget.focus) {
                 focus_widget.focus();
             }
@@ -2796,7 +2809,7 @@
         },
         get_visible: function() {
             // 480px is bootstrap's screen-xs-max
-            return ((window.visualViewport.width > 480) &&
+            return ((Sao.common.vp_width > 480) &&
                 !this.header.hasClass('invisible'));
         }
     });
@@ -3342,7 +3355,7 @@
             }
         },
         get_visible: function() {
-            return ((window.visualViewport.width > 480) &&
+            return ((Sao.common.vp_width > 480) &&
                 !this.header.hasClass('invisible'));
         },
         button_clicked: function(event) {
