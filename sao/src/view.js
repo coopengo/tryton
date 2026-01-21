9ef555a813a7bf909415713aa9b6d3e6bee80cd6
Nicolas Évrard <nicoe@openhex.org>
2026-01-21 22:38:47 +0100

    Use a more explicit error when a field is nonexistent in a view [CUSTOM]
    https://coopengo.atlassian.net/browse/PCLAS-1343

--- sao/src/view.js
+++ sao/src/view.js
@@ -208,6 +208,16 @@
                     }
                 }
             }
+
+            if (['field', 'label'].includes(node.tagName) &&
+                jQuery.isEmptyObject(field)) {
+                Sao.common.error.run(
+                    "Error",
+                    `Field '${node_attrs.name}' not defined ` +
+                    `in '${this.view.screen.model.name}'`);
+                throw new Error("Field Undefined");
+            }
+
             return node_attrs;
         },
         parse: function(node) {
