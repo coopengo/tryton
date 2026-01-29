79633f037d61178752e972077dba2efcc62a604f
Nicolas Évrard <nicoe@users.noreply.github.com>
2026-01-29 17:10:45 +0100

    Set focus on the first visible input when navigating with arrow keys [PREVIEW] (#580)
    https://coopengo.atlassian.net/browse/CCOOG-1781

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -2490,7 +2490,10 @@
                             }
                         }
                         this._get_column_td(next_column)
-                            .find(':input,[tabindex=0]').focus();
+                            .find(':input,[tabindex=0]')
+                            .filter(':visible')
+                            .first()
+                            .focus();
                     } else {
                         var prm = jQuery.when();
                         if (!this.tree.screen.group.parent) {
@@ -2539,7 +2542,10 @@
                                 this._get_column_td(
                                     next_column, next_row)
                                     .trigger('click')
+                                    .trigger('click')
                                     .find(':input,[tabindex=0]')
+                                    .filter(':visible')
+                                    .first()
                                     .focus();
                             });
                         }
