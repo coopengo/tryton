4aa9cfaabae8d51a98a15f054d8bdd1266c24110
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-25 09:43:23 +0100

    [sao][Main] Developer help [CUSTOM]
    Add developer help tooltips that display field metadata (model name, field
    type, states, domain, selection values) when hovering over form fields.
    Controlled by a developer_help flag in sao.js. Adds tooltip generation
    in view.js with integration points in form.js and tree.js widgets.
    
    Fix PCLAS-1456, PCLAS-1473
    
    Files: sao.js, view.js, view/form.js, view/tree.js
    Extraction ref: SA6-debug-C
    Source commits: a50a8ed, c8b3584

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -250,7 +250,10 @@
                         label.addClass('editable');
                     }
                 }
-                if (column.attributes.help) {
+                if ((Sao.config.developer_help) && (
+                        column.attributes.developer_help)) {
+                    label.attr('title', column.attributes.developer_help);
+                } else if (column.attributes.help) {
                     label.attr('title', column.attributes.help);
                 }
                 if (column.sortable) {
