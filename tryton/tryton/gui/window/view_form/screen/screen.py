bfff6c6d8b291530f844c014ca2ed0bfce28b0fd
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-25 14:42:40 +0100

    [tryton] Reset current record of screen on record creation [PREVIEW] (#446)
    Ref: #446

--- tryton/tryton/gui/window/view_form/screen/screen.py
+++ tryton/tryton/gui/window/view_form/screen/screen.py
@@ -738,13 +738,14 @@ class Screen:
         if self.current_view and self.current_view.view_type == 'calendar':
             selected_date = self.current_view.get_selected_date()
         if self.current_view and not self.current_view.creatable:
-            self.switch_view(creatable=True)
+            self.switch_view(creatable=True, display=False)
             if not self.current_view.creatable:
                 return None
         if self.current_record:
             group = self.current_record.group
         else:
             group = self.group
+        self.current_record = None
         record = group.new(default, defaults=defaults)
         group.add(record, self.new_position)
         if previous_view.view_type == 'calendar':
