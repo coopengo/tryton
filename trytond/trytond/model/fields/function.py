f2172602b12a1307f525ceb44537b5146d110334
Léo LAIOLO <leo.laiolo@coopengo.com>
2026-09-09 11:27:05 +0200

    format

--- trytond/trytond/model/fields/function.py
+++ trytond/trytond/model/fields/function.py
@@ -161,7 +161,9 @@ class Function(Field):
                     and not isinstance(value, field._py_type)):
                 if field._type == 'binary' and isinstance(value, int):
                     pass
-                elif field._py_type in [datetime.date, datetime.datetime, datetime.time] and isinstance(value, str):
+                elif (field._py_type in [
+                        datetime.date, datetime.datetime, datetime.time]
+                        and isinstance(value, str)):
                     value = field._py_type.fromisoformat(value)
                 else:
                     value = field._py_type(value)
